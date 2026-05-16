1ef2ff19650f57d69f2f8f83d545283ae740a9c8
gk1wi <gk1wi444@gmail.com>
2026-05-15 20:15:42 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "yin-yang-v2";
+const CACHE_NAME = "yin-yang-1.0.4";
 
 const FILES_TO_CACHE = [
   "./",
@@ -8,10 +8,28 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(FILES_TO_CACHE))
   );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
